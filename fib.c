68f36fbe73d7c64f89dab79286a3ae0dd22e9dd7
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2020-04-23 10:38:39 -0700

    fixed crippling memory leak

--- fib.c
+++ fib.c
@@ -1,25 +1,31 @@
 #include <emscripten.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 EMSCRIPTEN_KEEPALIVE
-void fillMandelbrotImgBuffer(unsigned char *buf, int width, int height) {
-  const int maxIterations = 30;
-  double minRe = -2.0;
-  double maxRe = 1.0;
-  double minIm = -1.2;
+void fillMandelbrotImgBuffer(
+    unsigned char *buf,
+    int width,
+    int height,
+    double minRe,
+    double maxRe,
+    double minIm,
+    int maxIterations
+  ) {
   double maxIm = minIm + (maxRe - minRe) * height / width;
   double reFactor = (maxRe - minRe) / (width - 1);
   double imFactor = (maxIm - minIm) / (height - 1);
 
   for (int y = 0; y < height; ++y) {
-    double cIm = maxIm - y * reFactor;
+    double cIm = maxIm - y * imFactor;
     for (int x = 0; x < width; ++x) {
       double cRe = minRe + x * reFactor;
 
       double zRe = cRe;
       double zIm = cIm;
       bool inside = true;
-      for (int n = 0; n < maxIterations; ++n) {
+      int n;
+      for (n = 0; n < maxIterations; ++n) {
         double zReSquared = zRe * zRe;
         double zImSquared = zIm * zIm;
         if (zReSquared + zImSquared > 4) {
@@ -29,16 +35,16 @@ void fillMandelbrotImgBuffer(unsigned char *buf, int width, int height) {
         zIm = 2 * zRe * zIm + cIm;
         zRe = zReSquared - zImSquared + cRe;
       }
-      int i = 4 * width * (height - y) + 4 * x;
+      int i = 4 * width * (height - y - 1) + 4 * x;
       if (inside) {
         buf[i] = 0;
         buf[i + 1] = 0;
         buf[i + 2] = 0;
         buf[i + 3] = 255;
       } else {
-        buf[i] = 255;
-        buf[i + 1] = 255;
-        buf[i + 2] = 255;
+        buf[i] = 255 / maxIterations * n;
+        buf[i + 1] = 0;
+        buf[i + 2] = 0;
         buf[i + 3] = 255;
       }
     }
--- makefile
+++ makefile
@@ -1,4 +1,4 @@
 debug:
-	emcc -O0 -s EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap"]' fib.c -o fib.js
+	emcc -O0 -s EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap"]' -s ALLOW_MEMORY_GROWTH=1 fib.c -o fib.js
 release:
-	emcc -O2 -s EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap"]' fib.c -o fib.js
+	emcc -O2 -s EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap"]' -s ALLOW_MEMORY_GROWTH=1 fib.c -o fib.js
